b2646fde74c889beeb8adcd5c56d430a61a8315d
Bataltsev <clubbars79@yandex.ru>
2023-02-17 20:53:06 +0300

    add program

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Write("Введите строки, разделяя запятой : ");
+string[] A = Console.ReadLine().Split(",").ToArray();
+int x = 0;
+void PrintString(string[] A)
+{
+    Console.Write("[");
+    for (x = 0; x < A.Length; x++)
+    {
+        Console.Write(A[x]);
+        if (x < A.Length)
+        {
+            Console.Write(",");
+        }
+    }
+    Console.Write("] = ");
+}
+PrintString(A);
+void FindString(string[] A)
+{
+    Console.Write("[");
+    for (x = 0; x < A.Length; x++)
+    {
+        if (A[x].Length <= 3)
+        {
+            Console.Write(A[x]);
+            Console.Write(",");
+        }
+    }
+    Console.Write("]");
+}
+FindString(A);
